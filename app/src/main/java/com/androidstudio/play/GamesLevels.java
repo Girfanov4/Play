28b6d0ea6515c1487f1188e7e6fbdbe0f2607b31
Girfanov4 <vg757634@gmail.com>
2021-09-26 21:07:42 +0500

    firstLevel

--- app/src/main/java/com/androidstudio/play/GamesLevels.java
+++ app/src/main/java/com/androidstudio/play/GamesLevels.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,6 +24,20 @@ public class GamesLevels extends AppCompatActivity {
             public void onClick(View view) {
                 try {
                     Intent intent = new Intent(GamesLevels.this, MainActivity.class);
+                    startActivity(intent); finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+
+        TextView textView1 =(TextView)findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(GamesLevels.this, Level1.class);
                     startActivity(intent); finish();
                 }catch (Exception e){
 
---
+++ app/src/main/java/com/androidstudio/play/Level1.java
@@ -0,0 +1,18 @@
+package com.androidstudio.play;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Level1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.universal);
+        Window window =getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+}
