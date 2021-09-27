ddc7587dc5cdcf235ced10f2cae3c47f8c907b29
Girfanov4 <vg757634@gmail.com>
2021-09-27 15:37:15 +0500

    firstLevel(4)

--- app/src/main/java/com/androidstudio/play/Level1.java
+++ app/src/main/java/com/androidstudio/play/Level1.java
@@ -3,8 +3,10 @@ package com.androidstudio.play;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
 
@@ -12,7 +14,12 @@ public class Level1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+        final ImageView img_left = (ImageView)findViewById(R.id.img_left);
+        img_left.setClipToOutline(true);
+        final ImageView img_right = (ImageView)findViewById(R.id.img_right);
+        img_right.setClipToOutline(true);
         Window window =getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
     }
 }
