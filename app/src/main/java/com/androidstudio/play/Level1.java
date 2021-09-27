912a9e4761d27164e2f5ceabe0eb4dc7ca39bec4
Girfanov4 <vg757634@gmail.com>
2021-09-27 18:13:00 +0500

    Number

--- app/src/main/java/com/androidstudio/play/Level1.java
+++ app/src/main/java/com/androidstudio/play/Level1.java
@@ -15,18 +15,28 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class Level1 extends AppCompatActivity {
 
     Dialog dialog;
+    public int numLeft;
+    public int numRight;
+    Array array = new Array();
+    Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
+        TextView text_levels = findViewById(R.id.text_levels);
+        text_levels.setText(R.string.level1);
         final ImageView img_left = (ImageView) findViewById(R.id.img_left);
         img_left.setClipToOutline(true);
         final ImageView img_right = (ImageView) findViewById(R.id.img_right);
         img_right.setClipToOutline(true);
+        final TextView text_left = findViewById(R.id.Text_left);
+        final TextView text_right = findViewById(R.id.Text_right);
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         dialog = new Dialog(this);
@@ -67,16 +77,26 @@ public class Level1 extends AppCompatActivity {
 
                 }
             }
-            });
+        });
+        numLeft = random.nextInt(10);
+        img_left.setImageResource(array.images1[numLeft]);
+        text_left.setText(array.texts1[numLeft]);
+        numRight = random.nextInt(10);
+        while (numLeft == numRight) {
+            numRight = random.nextInt(10);
         }
-        @Override
-    public void onBackPressed(){
-            try {
-                Intent intent = new Intent(Level1.this, GamesLevels.class);
-                startActivity(intent);
-                finish();
-            } catch (Exception e) {
+        img_right.setImageResource(array.images1[numRight]);
+        text_right.setText(array.texts1[numRight]);
+    }
 
-            }
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent intent = new Intent(Level1.this, GamesLevels.class);
+            startActivity(intent);
+            finish();
+        } catch (Exception e) {
+
         }
     }
+}
