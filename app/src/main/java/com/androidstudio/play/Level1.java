a2f3f3ad26c76ce4cd50099a704d993ad40b1a6d
Girfanov4 <vg757634@gmail.com>
2021-09-27 16:44:24 +0500

    Dialog box

--- app/src/main/java/com/androidstudio/play/Level1.java
+++ app/src/main/java/com/androidstudio/play/Level1.java
@@ -2,24 +2,58 @@ package com.androidstudio.play;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Level1 extends AppCompatActivity {
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
-        final ImageView img_left = (ImageView)findViewById(R.id.img_left);
+        final ImageView img_left = (ImageView) findViewById(R.id.img_left);
         img_left.setClipToOutline(true);
-        final ImageView img_right = (ImageView)findViewById(R.id.img_right);
+        final ImageView img_right = (ImageView) findViewById(R.id.img_right);
         img_right.setClipToOutline(true);
-        Window window =getWindow();
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.previewdialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(false);
+        TextView btnclose = (TextView) dialog.findViewById(R.id.btnclose);
+        btnclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(Level1.this, GamesLevels.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception e) {
+                }
+                dialog.dismiss();
+            }
+            });
+        Button btncontinue = (Button) dialog.findViewById(R.id.btncontinue);
+        btncontinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            dialog.dismiss();
+            }
+        });
+        dialog.show();
+        }
     }
-}
